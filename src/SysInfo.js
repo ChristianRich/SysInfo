9fc316e50c4dd673e86d67fc8566c4fe4156aa0c
Christian Schlosrich <christian.schlosrich@make.com.au>
2013-05-15 13:25:27 +1000

    Update

--- src/SysInfo.js
+++ src/SysInfo.js
@@ -2,7 +2,7 @@
  * Displays system information overlay in the top left corner of the screen.
  * Currently screen resolution and user agent is included.
  *
- * SysInfo can be activated in three two ways:
+ * SysInfo can be activated in three ways:
  *
  * 1) From a query string. Just type 'sysinfo=true' in the URL and it will pop open (as long as the script is included in the page).
  *
@@ -21,10 +21,6 @@ var SysInfo = function(){
 
 	var remove = function(){
 
-		if(!container){
-			return;
-		}
-
 		if(container.parentNode){
 			container.parentNode.removeChild(container);
 		}
@@ -34,31 +30,39 @@ var SysInfo = function(){
 		window.removeEventListener('resize', onResize);
 	};
 
-	var parseQueryString = function(){
+	/**
+	 * Returns the query string as an object.
+	 */
+	var QueryStringUtil = function(){
+
+		this.getVal = function(value){
+			return dict[value];
+		};
+
+		this.hasKey = function(key){
+			return dict.hasOwnProperty(key);
+		};
 
 		var dict  = {},
-			decode = function (s) {
+
+			decode = function(s){
+				if(!s) return '';
 				return decodeURIComponent(s.replace(/\+/g, ' '));
 			},
+
 			queryString = location.search.substring(1),
 			keyValues = queryString.split('&');
 
 		for(var i in keyValues) {
 			var key = keyValues[i].split('=');
-
-			if(key.length > 1) {
-				dict[decode(key[0])] = decode(key[1]);
-			}
+			dict[decode(key[0])] = decode(key[1]);
 		}
-
-		return dict;
 	};
 
 	var container = document.createElement('div');
 	container.style.cssText = "position: fixed; line-height: 100%; zoom: reset; display: block; text-align: left; vertical-align: baseline; border: 0 none; z-index: 20000; top: 10px; left: 10px; font-family: 'Lucida Console'; color: white; Monaco, monospace; font-size: 11px; margin: 10px; padding: 5px; background-color: rgba(100, 100, 100, 0.9); width: 240px; max-width: 360px; min-width: 240px;-webkit-user-drag: none;-webkit-touch-callout: none;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;-o-user-select: none;user-select: none;";
 
 	var dimContainer = document.createElement('div');
-	dimContainer.id = 'dim';
 	dimContainer.style.cssText = 'padding-bottom: 5px; font-weight: bold';
 	container.appendChild(dimContainer);
 
@@ -69,13 +73,13 @@ var SysInfo = function(){
 	var ua = new UAParser().getResult();
 	sysContainer.appendChild(document.createTextNode(JSON.stringify(ua, null, 1)));
 
+	var qs = new QueryStringUtil();
+
 	container.onclick = function(){
 		remove();
 	};
 
-	var qs = parseQueryString();
-
-	if(qs['sysinfo'] && !!document.body){
+	if(qs.hasKey('s') && !!document.body && !container.parentNode){
 		document.body.appendChild(container);
 	}
 
